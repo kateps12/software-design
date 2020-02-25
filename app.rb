08b99a87eb2864d03ff84b39494244862dc79c1c
kateps12 <kateps12@gmail.com>
2020-02-25 02:53:23 +0000

    Week 8. Still needs work on new rsvps.

--- app.rb
+++ app.rb
@@ -14,3 +14,19 @@ after { puts; }                                                             #
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+
+get "/" do
+    puts events_table.all
+    @events = events_table.all
+    view "events"
+end
+
+get "/events/:id" do
+    @event = events_table.where(id: params[:id]).first  #<- params[:id] is the number that gets passed into the table, .to_s prints it to a string (but gives you unreadable data), .first is used in order to actually see the sequel adapter (will give you data as a hash)
+    view "event"
+end
+
+get "event/:id/rsvps/new" do
+    @event = events_table.where(id: params[:id]).first
+    view "new_rsvp"
+end
